b17e49cbbe7faba1731f2dbe2a09a3a38e805757
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-04-14 18:54:37 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -76,8 +76,9 @@ void PhysicalNumber::update(PhysicalNumber &number){
 	
 	if(number.unitTOconvert==u)
 		break;
-	}
 	u=number.unitTOconvert;
+	}
+	
 }
 
 bool PhysicalNumber::ConvertionCheck(PhysicalNumber* a, PhysicalNumber b){
